c506ee992d58ace4fd7c3077088b406235261e69
Rechol Kibebe <recholkibebe@gmail.com>
2025-05-28 11:14:28 +0300

    new changes

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-const lines = ["C is fun","Python is cool","javascript is amazing"]
+const lines = ["C is fun","Python is cool","JavaScript is amazing"]
 for (let i = 0; i <lines.length;i++){
 	console.log(lines[i]);
 }
