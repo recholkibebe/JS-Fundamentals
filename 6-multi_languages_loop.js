e564f3dcc5d605b178fdd3a74c29211b3b45ff78
Rechol Kibebe <recholkibebe@gmail.com>
2025-05-28 11:09:28 +0300

    for to print

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-const lines = ["C is fun","Python is cool","Javascript is amazing"]
+const lines = ["C is fun","Python is cool","javascript is amazing"]
 for (let i = 0; i <lines.length;i++){
 	console.log(lines[i]);
 }
